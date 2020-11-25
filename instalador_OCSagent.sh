9317c4b6231851980ef84e316c9787ef43e2b0db
tronco-chan <73654249+tronco-chan@users.noreply.github.com>
2020-11-25 12:36:54 +0100

    Update instalador_OCSagent.sh

--- instalador_OCSagent.sh
+++ instalador_OCSagent.sh
@@ -2,9 +2,10 @@
 
 #########################################################################################################
 ##Script de instalación de OCS Inventory UnixAgent											                               ##
-##Fecha: 23/11/2020                                                                                    ##
+##Fecha: 25/11/2020                                                                                    ##
 ##Versión 1.0:  Descarga del agente e instala automáticamente. Únicamente necesario elegir el SO.      ##
 ##				En caso de error asegurarse de tener acceso a todos los repositorios "non-free" y similares  ##
+##v1.1: binarios corregidos
 ##Autores:                                                                                             ##
 ##			Luis Mera Castro																		                                           ##
 ##			Rubén Míguez Bouzas										                                                         ##
@@ -29,7 +30,7 @@ function instalarCENTOS() {
   #añadimos repositorio
   su -c 'rpm -Uvh https://download.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
   #modulos minimos
-  yum install perl-XML-Simple perl-devel perl-Compress-Zlib perl-Net-IP perl-LWP perl-Digest-MD5 perl-Net-SSLeay perl-Data-UUID
+  yum install perl-XML-Simple perl-devel perl-Compress-Zlib perl-Net-IP perl-LWP perl-Digest-MD5 perl-Net-SSLeay perl-Data-UUID libxml-simple-perl
   #todos los modulos recomendados
   #yum install perl-Crypt-SSLeay perl-Net-SNMP perl-Proc-Daemon perl-Proc-PID-File perl-Sys-Syslog pciutils smartmontools monitor-edid
   ejecucionINSTALL
@@ -38,13 +39,15 @@ function instalarCENTOS() {
 function instalarDEBIAN() {
   mkdir /var/log/ocsinventoryagent-logsfolder/
   touch /var/log/ocsinventoryagent-logsfolder/ocsagent.log
-  apt install -y libcrypt-ssleay-perl libnet-ssleay-perl libnet-ip-perl libnet-netmask-perl libproc-daemon-perl libdata-uuid-perl libxml-simple-perl make nmap
+  apt install -y libcrypt-ssleay-perl libnet-ssleay-perl libnet-ip-perl libnet-netmask-perl libproc-daemon-perl libdata-uuid-perl libxml-simple-perl make nmap perl-XML-Simple libxml-simple-perl
   apt install -y libcrypt-ssleay-perl libnet-snmp-perl libproc-pid-file-perl libproc-daemon-perl net-tools libsys-syslog-perl pciutils smartmontools read-edid nmap libnet-netmask-perl
   ejecucionINSTALL
 }
 
 function ejecucionINSTALL() {
-  wget https://github.com/tronco-chan/UnixAgent/archive/v2.8.0.tar.gz
+	rm -r UnixAgent-2.8*
+  wget https://github.com/tronco-chan/UnixAgent/blob/master/UnixAgent-2.8.0.tar.gz?raw=true
+	mv UnixAgent-2.8.0.tar.gz?raw=true v2.8.0.tar.gz
   tar -xvzf v2.8.0.tar.gz
   cd UnixAgent-2.8.0
   sudo env PERL_AUTOINSTALL=1 perl Makefile.PL && make && make install && perl postinst.pl --nowizard --server=https://ocsng.altia.es --nossl --logfile=/var/log/ocsinventoryagent-logsfolder/ocsagent.log --crontab --now
